0681173958a72d51e3de4191313b4f807f678e55
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-08-21 13:32:06 +0400

    feat: semaphore solution

---
+++ run-in-order.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.Semaphore;
+
+class Foo {
+
+    Semaphore second;
+    Semaphore third;
+
+    public Foo() { 
+        second = new Semaphore(0);
+        third = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        second.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        second.acquire();
+        printSecond.run();
+        third.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        
+        // printThird.run() outputs "third". Do not change or remove this line.
+        third.acquire();
+        printThird.run();
+    }    
+    
+}
