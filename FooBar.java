01f6447fc11e97b037181c48e523a7527a343ac5
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-08-30 15:47:46 +0400

    feat: description of solution

--- FooBar.java
+++ FooBar.java
@@ -11,13 +11,18 @@ class FooBar {
         this.n = n;
     }
 
-    public void foo(Runnable printFoo) throws InterruptedException {
+    public void foo(Runnable printFoo) throws InterruptedException {        
+    /* we use lock to acquire a lock on following section of code, if you dont use it you will get "illegal monitor state exception"
+    * since without acquiring reentrantlock, the await() and signal() would not be used in within a block that is synchronized on the same lock.
+    */
         lock.lock();
         try{
             for (int i = 0; i < n; i++) {
+                // following code waits while its bar's turn
                 while(!turn){
                     condition.await();               
                 }
+                // following code runs runnable, sets turn to be false and signals condition so now it would be bar's turn
                     printFoo.run();
                     turn = false;
                     condition.signal();        
